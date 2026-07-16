4222fa7c22251682f47087d80e195a6f7961e35b
FloF05 <ffwiede@t-online.de>
2026-07-16 22:33:40 +0200

    Wettermodul erweitert

--- modules/weather/weather.js
+++ modules/weather/weather.js
@@ -1,69 +1,101 @@
-async function loadWeather() {
+function loadWeather()
+{
 
-    try {
+    fetch("api/weather.php")
 
-        const response =
-            await fetch("api/weather.php");
+        .then(response => response.json())
+
+        .then(data =>
+        {
+
+            if(data.error)
+            {
+                console.error(data.error);
+
+                return;
+            }
 
 
-        if (!response.ok) {
+            document.getElementById("weather-location")
+                .innerText =
+                data.name;
 
-            throw new Error(
-                "HTTP-Fehler: " + response.status
+
+            document.getElementById("weather-temp")
+                .innerText =
+                Math.round(data.main.temp)
+                + " °C";
+
+
+            document.getElementById("weather-description")
+                .innerText =
+                data.weather[0].description;
+
+
+            document.getElementById("weather-feels")
+                .innerText =
+                "Gefühlt: "
+                + Math.round(data.main.feels_like)
+                + " °C";
+
+
+            document.getElementById("weather-humidity")
+                .innerText =
+                "Luftfeuchtigkeit: "
+                + data.main.humidity
+                + " %";
+
+
+            document.getElementById("weather-wind")
+                .innerText =
+                "Wind: "
+                + data.wind.speed
+                + " m/s";
+
+
+            let icon =
+                data.weather[0].icon;
+
+
+            document.getElementById("weather-icon")
+                .innerHTML =
+                `<img src="https://openweathermap.org/img/wn/${icon}@2x.png">`;
+
+
+            let now =
+                new Date();
+
+
+            document.getElementById("weather-updated")
+                .innerText =
+                "Aktualisiert: "
+                + now.toLocaleTimeString(
+                    "de-DE",
+                    {
+                        hour: "2-digit",
+                        minute: "2-digit"
+                    }
+                );
+
+        })
+
+        .catch(error =>
+        {
+
+            console.error(
+                "Fehler beim Laden des Wetters:",
+                error
             );
 
-        }
-
-
-        const data =
-            await response.json();
-
-
-        console.log("Wetterdaten:", data);
-
-
-        document.getElementById("weather-temp")
-            .innerText =
-            Math.round(data.main.temp) + "°C";
-
-
-        document.getElementById("weather-description")
-            .innerText =
-            data.weather[0].description;
-
-
-        document.getElementById("weather-feels")
-            .innerText =
-            "Gefühlt: "
-            +
-            Math.round(data.main.feels_like)
-            +
-            "°C";
-
-
-    }
-
-    catch (error) {
-
-        console.error(
-            "Fehler beim Laden des Wetters:",
-            error
-        );
-
-
-        document.getElementById("weather-temp")
-            .innerText =
-            "Fehler";
-
-
-        document.getElementById("weather-description")
-            .innerText =
-            "Wetterdaten nicht verfügbar";
-
-
-    }
+        });
 
 }
 
 
 loadWeather();
+
+
+setInterval(
+    loadWeather,
+    600000
+);
--- modules/weather/weather.php
+++ modules/weather/weather.php
@@ -1,28 +1,33 @@
 <div class="weather-module">
 
-
-    <h2>
+    <h2 id="weather-location">
         Wetter
     </h2>
 
-
-    <div id="weather-icon">
-    </div>
-
+    <div id="weather-icon"></div>
 
     <div id="weather-temp">
         --
     </div>
 
-
     <div id="weather-description">
         --
     </div>
 
-
     <div id="weather-feels">
-        --
+        Gefühlt: --
     </div>
 
+    <div id="weather-humidity">
+        Luftfeuchtigkeit: --
+    </div>
+
+    <div id="weather-wind">
+        Wind: --
+    </div>
+
+    <div id="weather-updated">
+        Aktualisiert: --
+    </div>
 
 </div>
